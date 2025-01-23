50462a2229f44c550831f904726b04d4837861e5
Santiago Florez <santiagoflorezv23@gmail.com>
2025-01-23 12:01:23 -0500

    vitrinas

--- Coffe-Front/main/src/app/pages/authentication/authentication.routes.ts
+++ Coffe-Front/main/src/app/pages/authentication/authentication.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 
-import { AppSideLoginComponent } from './side-login/side-login.component';
+import { LoginComponent } from './side-login/login.component';
 import { AppSideRegisterComponent } from './side-register/side-register.component';
+import { AppSideLoginComponent } from './side-login/side-login.component';
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -9,8 +10,13 @@ export const AuthenticationRoutes: Routes = [
     children: [
       {
         path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'login2',
         component: AppSideLoginComponent,
       },
+      
       {
         path: 'register',
         component: AppSideRegisterComponent,
